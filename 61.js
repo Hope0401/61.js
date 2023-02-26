7e158b0ac64e1e08e784ff50beb6746b9d10f1e2
Hope0401 <113833937+Hope0401@users.noreply.github.com>
2023-02-26 22:00:02 +0300

    Create 61.js

---
+++ 61.js
@@ -0,0 +1,17 @@
+//Задание 1
+//Напишите функцию ucFirst(str), возвращающую строку str с заглавным первым символом.
+//Вам понадобятся методы строк.
+
+function ucFirst(str);
+let newucFirst = 's';
+console.log (newucFirst.toUpperCase(0) + ucFirst);
+
+
+function ucFirst(str);
+console.log (ucFirst.toUpperCase(0) + ucFirst);
+
+//Задание 2
+//Напишите функцию checkSpam(str), возвращающую true, если str содержит 'badWord' или 'XXX', а иначе false.
+//Функция должна быть нечувствительна к регистру.
+
+function checkSpam(str);
